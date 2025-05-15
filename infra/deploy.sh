d79e82125d508358708cf097d9295822f5c34e36
Emmanuel Atawodi <gmemmy22@gmail.com>
2025-05-15 18:23:27 +0100

    chore: deploy cloud formation stack

---
+++ infra/deploy.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+if [ -f ../.env ]; then
+  export $(grep -v '^#' ../.env | xargs)
+else
+  echo ".env not found. please create one based on the .env.example file"
+  exit 1
+fi
+
+ACCOUNT_ID=$(aws sts get-caller-identity \
+  --profile $AWS_PROFILE \
+  --region $AWS_REGION \
+  --query "Account" \
+  --output text)
+
+echo "Deploying NebulaStream [$ENV] in account $ACCOUNT_ID / region $AWS_REGION"
+
+echo "Debug: Command parameters: --parameter-overrides Environment=$ENV CryptoApiKey='$CRYPTO_KEY'"
+
+aws cloudformation deploy \
+  --profile $AWS_PROFILE \
+  --region $AWS_REGION \
+  --stack-name nebula-stream-${ENV} \
+  --template-file nebula-stream.yml \
+  --parameter-overrides Environment=$ENV CryptoApiKey=$CRYPTO_KEY \
+  --capabilities CAPABILITY_NAMED_IAM
+
+echo "nebula-stream-$ENV stack deployed successfully"
+  
